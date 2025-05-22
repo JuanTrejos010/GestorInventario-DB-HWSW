b7eb5928a73330a2f8490f15a80c4424068791e1
Juan Trejos <79339661+JuanTrejos010@users.noreply.github.com>
2025-05-21 20:41:33 -0500

    Update tablas.py

--- tablas.py
+++ tablas.py
@@ -1,10 +1,16 @@
+#Librerías
 from sqlmodel import Field, SQLModel, create_engine
+from datetime import date
 
+#Clase para añadir Inventario
 class Inventario(SQLModel, table=true):
-  id: Optional[int] = Field(default=None, index=True, primary_key=True)
+  id: int= Field(default=None, index=True, primary_key=True)
   Nombre:str
   Descripcion:str
   Salon:str
   LugCompra:str
+  FechaCompra:date
+  Estado:string
+  
   
   
