4b399aa3d301699d11004cd504cdec21fe811d68
Juan Trejos <79339661+JuanTrejos010@users.noreply.github.com>
2025-05-21 20:31:09 -0500

    Añadiendo más atributos

--- tablas.py
+++ tablas.py
@@ -1,6 +1,10 @@
 from sqlmodel import Field, SQLModel, create_engine
 
 class Inventario(SQLModel, table=true):
-  id: Optional[int] = Field(default=None, primary_key=True)
+  id: Optional[int] = Field(default=None, index=True, primary_key=True)
   Nombre:str
+  Descripcion:str
+  Salon:str
+  LugCompra:str
+  
   
