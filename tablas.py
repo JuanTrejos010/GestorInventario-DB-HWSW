c31c4affc1d42831d9cef7c5e9e2b9d852056cb0
Juan Trejos <79339661+JuanTrejos010@users.noreply.github.com>
2025-05-21 20:56:07 -0500

    Update tablas.py

--- tablas.py
+++ tablas.py
@@ -11,6 +11,16 @@ class Inventario(SQLModel, table=true):
   LugCompra:str
   FechaCompra:date
   Estado:string
+
+'''
+@app.post("/submit")
+async def guardar_usuario(
+    nombre: str = Form(...),
+    fecha_nacimiento: str = Form(...)
+):
+    # Convertir string a objeto date
+    fecha = datetime.strptime(fecha_nacimiento, "%Y-%m-%d").date()
+'''
   
   
   
